9e755e7a46302e6797e3e43335e80e0b6610770e
Neal Robben <neal.robben@gmail.com>
2020-09-11 18:25:50 +0200

    Added Delete-commands for Department/Course

---
+++ src/Application/Courses/Commands/DeleteCourse/DeleteCourseCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using ContosoUniversityAngular.Application.Common.Exceptions;
+using ContosoUniversityAngular.Application.Common.Interfaces;
+using ContosoUniversityAngular.Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityAngular.Application.Courses.Commands.DeleteCourse
+{
+    public class DeleteCourseCommandHandler : IRequestHandler<DeleteCourseCommand>
+    {
+        private readonly ISchoolContext _context;
+
+        public DeleteCourseCommandHandler(ISchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = await _context.Courses.FindAsync(request.ID);
+
+            if (course == null)
+                throw new NotFoundException(nameof(Course), request.ID);
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Application/Departments/Commands/DeleteDepartment/DeleteDepartmentCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ContosoUniversityAngular.Application.Departments.Commands.DeleteDepartment
+{
+    public class DeleteDepartmentCommand : IRequest
+    {
+        public int ID { get; set; }
+
+        public DeleteDepartmentCommand(int id)
+        {
+            ID = id;
+        }
+    }
+}

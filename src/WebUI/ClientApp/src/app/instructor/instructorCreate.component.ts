ca945c10fce850ebdae991e7a851371569d1cf12
Neal Robben <neal.robben@gmail.com>
2020-09-23 23:11:02 +0200

    Implemented Create Instructor command in Angular

--- src/WebUI/ClientApp/src/app/instructor/instructorCreate.component.ts
+++ src/WebUI/ClientApp/src/app/instructor/instructorCreate.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+
+import { InstructorsClient, CreateInstructorCommand } from '../contosouniversityangular-api';
 
 @Component({
   selector: 'instructor-create',
   templateUrl: './instructorCreate.component.html'
 })
 export class InstructorCreateComponent {
-  title = 'Instructor create';
+
+  public instructor: CreateInstructorCommand = new CreateInstructorCommand();
+
+  constructor(private client: InstructorsClient) {
+  }
+
+  save(form: NgForm) {
+
+    if (form.valid) {
+      this.client.create(this.instructor).subscribe(result => {
+        this.router.navigateByUrl("/instructors");
+      }, error => console.error(error));
+    }
+    else {
+      console.log("form invalid");
+    }
+  }
+
 }

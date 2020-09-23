d67b1bb6fabeea47228ccf76dd85b9a9d9d38d9d
Neal Robben <neal.robben@gmail.com>
2020-09-23 23:23:24 +0200

    Added command for Create Student

--- src/WebUI/ClientApp/src/app/student/studentCreate.component.ts
+++ src/WebUI/ClientApp/src/app/student/studentCreate.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+
+import { StudentsClient, CreateStudentCommand } from '../contosouniversityangular-api';
 
 @Component({
   selector: 'student-create',
   templateUrl: './studentCreate.component.html'
 })
 export class StudentCreateComponent {
-  title = 'Student create';
+
+  public student: CreateStudentCommand = new CreateStudentCommand();
+
+  constructor(private client: StudentsClient) {
+  }
+
+  save(form: NgForm) {
+
+    if (form.valid) {
+      this.client.create(this.student).subscribe(result => {
+        this.router.navigateByUrl("/students");
+      }, error => console.error(error));
+    }
+    else {
+      console.log("form invalid");
+    }
+  }
+
 }

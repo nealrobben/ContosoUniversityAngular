ca28c8a01cd958548d2f172ec0e6077d71eb695e
Neal Robben <neal.robben@gmail.com>
2020-10-04 22:11:23 +0200

    Added server-side command to get Students for Course Enrolment

---
+++ src/Application/Students/Queries/GetStudentsForCourse/GetStudentsForCourseQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ContosoUniversityAngular.Application.Students.Queries.GetStudentsForCourse
+{
+    public class GetStudentsForCourseQuery : IRequest<StudentsForCourseVM>
+    {
+        public int? ID { get; set; }
+
+        public GetStudentsForCourseQuery(int? id)
+        {
+            ID = id;
+        }
+    }
+}
---
+++ src/Application/Students/Queries/GetStudentsForCourse/StudentForCourseVM.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ContosoUniversityAngular.Application.Common.Mappings;
+using ContosoUniversityAngular.Domain.Entities;
+using ContosoUniversityAngular.Domain.Enums;
+
+namespace ContosoUniversityAngular.Application.Students.Queries.GetStudentsForCourse
+{
+    public class StudentForCourseVM : IMapFrom<Enrollment>
+    {
+        public string StudentName { get; set; }
+        public Grade? StudentGrade { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Enrollment, StudentForCourseVM>()
+                .ForMember(d => d.StudentName, opt => opt.MapFrom(s => s.Student.FullName))
+                .ForMember(d => d.StudentGrade, opt => opt.MapFrom(s => s.Grade));
+        }
+    }
+}

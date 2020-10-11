6bc4cf265481113f2b94a23171521c3c08d92baa
Neal Robben <neal.robben@gmail.com>
2020-10-11 23:13:36 +0200

    Started implementing client side of Students overview

--- src/WebUI/ClientApp/src/app/student/studentOverview.component.ts
+++ src/WebUI/ClientApp/src/app/student/studentOverview.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { StudentsClient, StudentsOverviewVM } from '../contosouniversityangular-api';
 
 @Component({
   selector: 'student-overview',
   templateUrl: './studentOverview.component.html'
 })
 export class StudentOverviewComponent {
+
+  public vm: StudentsOverviewVM = new StudentsOverviewVM();
   title = 'Students';
+
+  constructor(private client: StudentsClient) {
+    this.getStudents();
+  }
+
+  public deleteStudent(id: string, name: string) {
+    if (confirm("Delete student: " + name + "?")) {
+      this.client.delete(id).subscribe(result => {
+        this.getStudents();
+      }, error => console.error(error));
+    }
+  }
+
+  private getStudents() {
+    console.log("getStudents");
+    //this.client.getAll().subscribe(result => {
+    //  this.vm = result;
+    //}, error => console.error(error));
+  }
+
 }

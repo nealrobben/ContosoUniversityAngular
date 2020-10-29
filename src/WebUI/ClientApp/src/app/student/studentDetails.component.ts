510179d27cfc9e8fbc09a465b4716dc41f8f3e7e
Neal Robben <neal.robben@gmail.com>
2020-10-29 22:29:05 +0100

    Finished client side of Student details

--- src/WebUI/ClientApp/src/app/student/studentDetails.component.ts
+++ src/WebUI/ClientApp/src/app/student/studentDetails.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { StudentsClient, StudentDetailsVM, Grade } from '../contosouniversityangular-api';
 
 @Component({
   selector: 'student-details',
   templateUrl: './studentDetails.component.html'
 })
 export class StudentDetailsComponent {
+
   title = 'Students';
+
+  public vm: StudentDetailsVM = new StudentDetailsVM();
+
+  constructor(private client: StudentsClient, activeRoute: ActivatedRoute) {
+
+    var studentId = activeRoute.snapshot.params["id"];
+
+    client.get(studentId).subscribe(result => {
+      this.vm = result;
+    }, error => console.error(error));
+  }
+
+  get Grade() { return Grade; }
+
 }
